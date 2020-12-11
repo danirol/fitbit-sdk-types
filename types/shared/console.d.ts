2789b7860a7d499e0cd0161dec2b307f18de4cee
danirol <73183380+danirol@users.noreply.github.com>
2020-12-10 20:46:30 -0600

    Delete console.d.ts

--- types/shared/console.d.ts
+++
@@ -1,9 +0,0 @@
-interface Console {
-	assert(assertion: boolean, message?: any, ...optionalParams: any[]): void;
-	error(message?: any, ...optionalParams: any[]): void;
-	info(message?: any, ...optionalParams: any[]): void;
-	log(message?: any, ...optionalParams: any[]): void;
-	warn(message?: any, ...optionalParams: any[]): void;
-	trace(): void;
-}
-declare const console: Console;
